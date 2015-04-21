ae7862f7cdadb94466714ed73300f8e9851765ad
Mikael Janhonen <janhon3n@gmail.com>
2015-04-21 20:49:18 +0300

    Update eng.php

--- lan/eng.php
+++ lan/eng.php
@@ -1,18 +1,33 @@
 <?
 $LAN = (object) array(
         'frontpagetitle' => 'JanhoSlide',
-	'frontpagedefaultlink' => 'Default show',
-	'frontpagecustomlink' => 'Custom show',
+        'frontpagedefaultlink' => 'Default show',
+        'frontpagecustomlink' => 'Custom show',
+        'frontpagedatalink' => 'Edit data',
         'defaultpagetitle' => 'JanhoSlide default',
         'custompagetitle' => 'JanhoSlide custom',
-	'restitle' => 'Resolution',
-	'reswidth' => 'Width',
-	'resheight' => 'Height',
-	'reskeepratio' => 'Keep the 16:9 aspect ratio',
-	'timetitle' => 'Time values',
-	'timeswitchtime' => 'Switchtime',
-	'timeswitchinfo' => 'How long does the switch take.',
-	'timeshowtime' => 'Showtime',
-	'timeshowinfo' => 'How long one slide is shown.',
-	'switchtitle' => 'Switching style',
-	'readybutton' => 'Ready');
+        'datapagetitle' => 'JanhoSlide',
+        'restitle' => 'Resolution',
+        'reswidth' => 'Width',
+        'resheight' => 'Height',
+        'reskeepratio' => 'Force 16:9 aspect ratio',
+        'timetitle' => 'Time values',
+        'timeswitchtime' => 'Switchtime',
+        'timeswitchinfo' => 'How long does the switch take.',
+        'timeshowtime' => 'Showtime',
+        'timeshowinfo' => 'How long one slide is shown.',
+        'switchtitle' => 'Switching style',
+        'readybutton' => 'Ready',
+        'datapagesendnewheader' => 'Send new file:',
+        'datapageimagesheader' => 'Images:',
+        'datapagevideosheader' => 'Videos:',
+        'datapageelementoptionsshowfull' => 'Open fullsize',
+        'datapageelementoptionsdelete' => 'Move to archive');
+
+
+
+
+
+
+
+
